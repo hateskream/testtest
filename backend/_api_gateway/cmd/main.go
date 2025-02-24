5036eb177fe4c6fb5442260080dccdc2f2b1a1f1
Nikolay Borovik <nborovik@planet9.cc>
2025-02-24 11:42:03 +0500

    clean up

--- backend/_api_gateway/cmd/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "gitlab.dev-api.tech/data_loader_service/internal/app"
-
-func main() {
-
-	app.Run()
-}
--- backend/_api_gateway/internal/domain/fetcher/fetcher.go
+++
@@ -1,7 +0,0 @@
-package fetcher
-
-import "gitlab.dev-api.tech/data_loader_service/internal/domain/fetcher_client"
-
-type Fetcher interface {
-	GetFectherClient() (fetcher_client.FetcherClient, error)
-}
