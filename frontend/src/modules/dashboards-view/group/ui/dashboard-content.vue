7f97ac650309e4b089e8f2cc00ed632daea3c266
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-03-12 12:58:11 +0300

    add base impl dashboard magment

---
+++ frontend/src/modules/dashboards-view/group/ui/dashboard-content.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+import { DashboardType } from '../model/types';
+import { FearGreedDashboard } from '../../dashboards/fear-greed';
+import { MarketDashboard } from '../../dashboards/market';
+import { MarketCapDashboard } from '../../dashboards/market-cap';
+import { NewsDashboard } from '../../dashboards/news';
+import { PriceDashboard } from '../../dashboards/price';
+import { useDashboardsStore } from '../model/dashboards-store';
+
+const { activeGroup } = storeToRefs(useDashboardsStore());
+
+function getDashboardComponent(type: DashboardType) {
+	const components = {
+		[DashboardType.FearGreed]: FearGreedDashboard,
+		[DashboardType.Market]: MarketDashboard,
+		[DashboardType.MarketCap]: MarketCapDashboard,
+		[DashboardType.News]: NewsDashboard,
+		[DashboardType.Price]: PriceDashboard,
+	};
+	return components[type];
+}
+</script>
+
+<template>
+	<div :class="classes.dashboardsContainer">
+		<div
+			v-if="activeGroup"
+			:class="classes.dashboardGrid"
+		>
+			<component
+				:is="getDashboardComponent(dashboard.type)"
+				v-for="dashboard in activeGroup.dashboards"
+				:key="dashboard.id"
+				:dashboard="dashboard"
+			/>
+		</div>
+	</div>
+</template>
+
+<style module="classes">
+.dashboardsContainer {
+	padding: 20px;
+	border: 1px solid #eeeeee;
+}
+
+.dashboardGrid {
+	display: grid;
+	gap: 20px;
+	margin-bottom: 20px;
+}
+</style>
