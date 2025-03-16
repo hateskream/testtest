7407c61d3f289a14a0dad82a82dd5883b50ffa40
Iiya Baikov <i.baikov@planet9.cc>
2025-03-16 18:54:20 +0000

    Feat/add base colors fonts

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
+import '../assets/styles/base.css';
+
 import routes from './routes';
 
 import App from './app.vue';
