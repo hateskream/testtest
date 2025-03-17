9a6743526f32b4b58508f2bbb30bdb0c9cef3ca1
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-03-17 13:49:29 +0300

    create icon component

---
+++ frontend/src/shared/ui/icon/icon-placeholder.vue
@@ -0,0 +1,10 @@
+<template>
+	<div :class="classes.root" />
+</template>
+
+<style module="classes">
+.root {
+	width: 100%;
+	height: 100%;
+}
+</style>
---
+++ frontend/src/shared/ui/icon/icons.ts
@@ -0,0 +1,3 @@
+export enum IconIds {
+	Logo = 'logo',
+}
