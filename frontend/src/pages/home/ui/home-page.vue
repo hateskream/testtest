88d813d9523c46895bee8a87b07cc870f5d130d0
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-03-18 19:49:03 +0300

    delete test components

--- frontend/src/pages/home/ui/home-page.vue
+++ frontend/src/pages/home/ui/home-page.vue
@@ -1,79 +1,14 @@
 <script setup lang="ts">
-import {
-	AllDashboard,
-	DashboardContent,
-	TabsComponent,
-} from '@/modules/dashboards-view/group';
 import { LayoutComponent } from '@/modules/layaout';
 </script>
 
 <template>
 	<layout-component>
 		<template #header>
-			<tabs-component />
+			<div />
 		</template>
 		<template #content>
-			<all-dashboard />
-			<dashboard-content />
-			<div :style="{ color: 'black' }">
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-				<div>dffff</div>
-			</div>
+			<div />
 		</template>
 	</layout-component>
 </template>
