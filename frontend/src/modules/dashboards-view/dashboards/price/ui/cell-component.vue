895c77ae27d8e3ba6b379dcdde1a19fcae5e679c
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-03-26 20:23:30 +0300

    add base ui component

---
+++ frontend/src/modules/dashboards-view/dashboards/price/ui/cell-component.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+import type { ICurrency } from '../model';
+import { usePriceStore } from '../stores';
+import { UiImage } from '@/shared/ui/image';
+
+interface ICellComponentProps {
+	currency: ICurrency;
+}
+
+const props = defineProps<ICellComponentProps>();
+
+const { isShowChart, isShowPercentageChange, isShowLogo, isShowTicker } =
+	storeToRefs(usePriceStore());
+</script>
+
+<template>
+	<div :class="classes.root">
+		<div
+			v-if="isShowLogo"
+			:class="classes.logo"
+		>
+			<ui-image :src="props.currency.srcImage" />
+		</div>
+		<div :class="classes.container">
+			<div v-if="isShowTicker">{{ props.currency.ticker }}</div>
+			<div :class="classes.containerSecond">
+				<div :class="classes.marketCap">{{ props.currency.marketCap }}</div>
+				<div v-if="isShowPercentageChange">{{ props.currency.changeLastDay }}</div>
+			</div>
+		</div>
+		<div v-if="isShowChart">
+			<div :class="classes.chart">chart</div>
+		</div>
+	</div>
+</template>
+
+<style module="classes"></style>
--- frontend/src/modules/dashboards-view/dashboards/price/ui/view-component.vue
+++ frontend/src/modules/dashboards-view/dashboards/price/ui/view-component.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-
 import type { ICurrency } from '../model';
-import { usePriceStore } from '../stores';
+
+import CellComponent from './cell-component.vue';
 
 interface IViewComponentProps {
 	currencies: ICurrency[];
 }
 
 const props = defineProps<IViewComponentProps>();
-
-const { isShowChart, isShowPercentageChange, isShowLogo, isShowTicker } =
-	storeToRefs(usePriceStore());
 </script>
 
-<template>data</template>
+<template>
+	<div>
+		<cell-component
+			v-for="currency in props.currencies"
+			:key="currency.ticker"
+			:currency="currency"
+		/>
+	</div>
+</template>
+
+<style module="classes"></style>
