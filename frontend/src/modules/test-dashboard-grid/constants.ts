413322013e87ed5cc42cf3a5baaf926f214f1121
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-04-22 07:30:23 +0300

    fix min/max constants to create grid item

--- frontend/src/modules/test-dashboard-grid/constants.ts
+++ frontend/src/modules/test-dashboard-grid/constants.ts
@@ -1,5 +1,5 @@
-export const MIN_COL_WIDTH = 150;
-export const MAX_COL_WIDTH = 225;
-export const MIN_ROW_HEIGHT = 104;
-export const MAX_ROW_HEIGHT = 140;
+export const MIN_COL_WIDTH = 156;
+export const MAX_COL_WIDTH = 231;
+export const MIN_ROW_HEIGHT = 55;
+export const MAX_ROW_HEIGHT = 73;
 export const GAP = 6;
