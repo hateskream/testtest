22f6930f578dec845c825b957f74ad65dd069e61
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-04-22 08:11:29 +0300

    fix

--- frontend/src/modules/test-dashboard-grid/test-component.vue
+++ frontend/src/modules/test-dashboard-grid/test-component.vue
@@ -417,8 +417,6 @@ function updateLayout(newLayout: Layout) {
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-
-	/* width: 100%; */
 	min-height: 100%;
 }
 
@@ -428,18 +426,8 @@ function updateLayout(newLayout: Layout) {
 
 .root {
 	position: relative;
-
-	/* display: flex; */
 	flex-grow: 1;
-
-	/* justify-content: center; */
-
-	/* align-items: center; */
 	width: 100%;
-
-	/* min-height: 100%; */
-
-	/* overflow: hidden; */
 }
 
 .content {
@@ -449,8 +437,6 @@ function updateLayout(newLayout: Layout) {
 
 .grid {
 	position: absolute;
-
-	/* z-index: -1; */
 	width: 100%;
 	height: 100%;
 }
