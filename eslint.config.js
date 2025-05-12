05bc5e9489e11e59c725244f928be5b4ab208e58
Mark Solovev <msolovjev@planet9.cc>
2025-05-12 19:56:14 +0500

    feat: add ESLint rule for component name casing in Vue templates

--- eslint.config.js
+++ eslint.config.js
@@ -150,6 +150,10 @@ export default [
 				],
 				'alphabetical': false
 			}],
+			'vue/component-name-in-template-casing': ['error', 'kebab-case', {
+				'registeredComponentsOnly': false,
+				'ignores': []
+			}],
 
 			// TypeScript
 			'@typescript-eslint/no-shadow': 'error',
