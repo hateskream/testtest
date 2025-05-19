a7a3e55dfa5b32b913f1fa5ecee0517de3a63431
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-05-19 17:48:25 +0300

    feat: add layout curtain in open state

--- src/modules/dashboards-curtain/ui/dashboards-curtain-component.vue
+++ src/modules/dashboards-curtain/ui/dashboards-curtain-component.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import type { IDashboardInstance } from '@/modules/dashboard-group';
+import { IconIds, UiIcon } from '@/shared/ui/icon';
 
 import DraggableElement from './draggable-element.vue';
 import DashboardComponent from './dashboard-component.vue';
-import DeleteComponent from './delete-component.vue';
+// import DeleteComponent from './delete-component.vue';
 
 
 interface IDashboardsCurtainComponentProps {
@@ -30,41 +31,104 @@ function fixCurtain() {
 	isCurtainFixed.value = true;
 }
 
-function unfixCurtain() {
-	isCurtainFixed.value = false;
-}
+// function unfixCurtain() {
+// 	isCurtainFixed.value = false;
+// }
 </script>
 
 <template>
-	<div :class="classes.root">
-		<draggable-element
-			v-for="dashboard in props.dashboards"
-			:key="dashboard.id"
-			@drag="onDrag(dashboard)"
-			@drag-end="emit('drag-end')"
-		>
-			<template #content>
-				<dashboard-component :title="dashboard.name" />
-			</template>
-			<template #ghost>
-				<slot name="ghost" :title="dashboard.name" />
-			</template>
-		</draggable-element>
-		<delete-component @set-can-delete="emit('set-can-delete', $event)" />
+	<div :class="[classes.root, { [classes.notFixed]: !isCurtainFixed }]">
+		<div :class="classes.container">
+			<div :class="classes.content">
+				<draggable-element
+					v-for="dashboard in props.dashboards"
+					:key="dashboard.id"
+					@drag="onDrag(dashboard)"
+					@drag-end="emit('drag-end')"
+				>
+					<template #content>
+						<dashboard-component :title="dashboard.name" />
+					</template>
+					<template #ghost>
+						<slot name="ghost" :title="dashboard.name" />
+					</template>
+				</draggable-element>
+			</div>
+			<div v-if="!isCurtainFixed" :class="classes.panel">
+				<div
+					ref="curtainIconRef"
+					:class="classes.iconWrapper"
+					@click="fixCurtain"
+				>
+					<ui-icon
+						:id="IconIds.ControlRightMenu"
+						width="20px"
+						height="20px"
+					/>
+				</div>
+
+				<div
+					ref="addWidgetIconRef"
+					:class="classes.iconWrapper"
+					@click="fixCurtain"
+				>
+					<ui-icon
+						:id="IconIds.AddWidget"
+						width="20px"
+						height="20px"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- <delete-component @set-can-delete="emit('set-can-delete', $event)" />
 		<div @click="fixCurtain">
 			fix
 		</div>
 		<div @click="unfixCurtain">
 			unfix
-		</div>
-	</div>
+		</div> -->
 </template>
 
 <style module="classes">
 .root {
+	width: 358px;
+	height: 100%;
+}
+
+.notFixed {
+	background-color: var(--bg-modal-color-base);
+	border: 1px solid var(--border-modal-color-base);
+	border-radius: 18px;
+}
+
+.container {
 	display: flex;
+	height: 100%;
+}
+
+.content {
+	display: flex;
+	flex-grow: 1;
 	flex-direction: column;
 	gap: 10px;
-	align-items: end;
+	padding: 18px 12px 0 8px;
+}
+
+.panel {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+}
+
+.iconWrapper {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 48px;
+	height: 48px;
+	color: var(--icon-color-base-500);
+	cursor: pointer;
 }
 </style>
