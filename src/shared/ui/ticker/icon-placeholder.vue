7d507778c1e1ed4a288dad0e77ccb15d3f8f28aa
Mark Solovev <msolovjev@planet9.cc>
2025-06-04 18:35:32 +0500

    feat(ticker): add component for ticker logo placeholder

---
+++ src/shared/ui/ticker/icon-placeholder.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+interface IIconPlaceholderProps {
+	ticker: string;
+}
+
+const props = defineProps<IIconPlaceholderProps>();
+
+const tickerLetter = computed(() => {
+	return props.ticker.charAt(0);
+});
+</script>
+
+<template>
+	<div :class="classes.iconPlaceholder">
+		{{ tickerLetter }}
+	</div>
+</template>
+
+<style module="classes">
+.iconPlaceholder {
+	flex-shrink: 0;
+	width: 24px;
+	font-style: normal;
+	font-weight: 390;
+	font-size: var(--typography-headers-size-h00);
+	text-align: center;
+	color: var(--text-color-base-300);
+	letter-spacing: 0.075px;
+}
+</style>
