2d69408209a93cf5f3fc808979404b106ebdacb7
Mark Solovev <msolovjev@planet9.cc>
2025-07-07 16:50:28 +0500

    refactor(performance-loader): use skeleton group and css modules
    - replace multiple ui-skeleton instances with ui-skeleton-group component
    - migrate component styles to css modules for better encapsulation

--- src/modules/widgets/performance/ui/layouts/performance-loader.vue
+++ src/modules/widgets/performance/ui/layouts/performance-loader.vue
@@ -1,27 +1,21 @@
 <script setup lang="ts">
-import { UiSkeleton } from '@/shared/ui/skeleton';
+import { UiSkeletonGroup, UiSkeleton } from '@/shared/ui/skeleton';
 </script>
 
 <template>
-	<div class="preloader-container">
-		<div class="preloader-header">
+	<div :class="classes.preloaderContainer">
+		<div :class="classes.preloaderHeader">
 			<ui-skeleton width="30%" height="24px" />
 			<ui-skeleton width="60px" height="24px" />
 		</div>
-		<div class="preloader-content">
-			<ui-skeleton
-				v-for="i in 8"
-				:key="i"
-				width="100%"
-				height="30px"
-				:style="{ marginBottom: '2px' }"
-			/>
+		<div :class="classes.preloaderContent">
+			<ui-skeleton-group :count="8" height="40px" />
 		</div>
 	</div>
 </template>
 
-<style scoped>
-.preloader-container {
+<style module="classes">
+.preloaderContainer {
 	display: flex;
 	flex-direction: column;
 	height: 100%;
@@ -29,13 +23,13 @@ import { UiSkeleton } from '@/shared/ui/skeleton';
 	gap: 16px;
 }
 
-.preloader-header {
+.preloaderHeader {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 }
 
-.preloader-content {
+.preloaderContent {
 	flex: 1;
 }
 </style>
