919c9d14f256697c396b709bbde8ab875684f1dd
Mark Solovev <msolovjev@planet9.cc>
2025-07-09 19:40:43 +0500

    feat(ui): add info tooltip component
    - introduce a reusable component for displaying contextual information via a tooltip
    - wraps UiTooltip and UiIcon to provide a standardized info icon with hover functionality
    - includes basic styling for the icon and its hover state

---
+++ src/shared/ui/info-tooltip/info-tooltip.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { UiTooltip } from '@/shared/ui/tooltip';
+import { IconIds, UiIcon } from '@/shared/ui/icon';
+
+interface IInfoTooltipProps {
+	text: string;
+}
+
+const props = defineProps<IInfoTooltipProps>();
+</script>
+
+<template>
+	<ui-tooltip
+		:show-in-ms="10"
+		position="top"
+		:class="classes.tooltipWrapper"
+	>
+		<template #default>
+			<div :class="classes.iconWrapper">
+				<ui-icon
+					:id="IconIds.Info"
+					width="6px"
+					height="6px"
+					:class="classes.icon"
+				/>
+			</div>
+		</template>
+		<template #content>
+			<slot>{{ props.text }}</slot>
+		</template>
+	</ui-tooltip>
+</template>
+
+<style module="classes">
+.tooltipWrapper {
+	cursor: pointer;
+
+	&:hover {
+		.iconWrapper {
+			color: var(--text-color-base-500);
+		}
+	}
+}
+
+.iconWrapper {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 12px;
+	height: 12px;
+	font-size: 8px;
+	line-height: 1;
+	color: var(--text-color-base-300);
+	background-color: rgb(217 217 217 / 20%);
+	border-radius: 99999px;
+	cursor: pointer;
+	transition: all 0.2s ease-in-out;
+	user-select: none;
+}
+
+</style>
