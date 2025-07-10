07ed11c2b75f6cb22e36e99525b0985a717c51d3
Mark Solovev <msolovjev@planet9.cc>
2025-07-10 16:30:32 +0500

    feat(altcoin-season): add time filter for btc performance heading
    - integrate `UiPosition` component to display `AltcoinSeasonTimeFilter` modal
    - implement icon rotation for dropdown indicator based on modal visibility

--- src/modules/widgets/altcoinSeason/ui/btc-performance/btc-performance-heading.vue
+++ src/modules/widgets/altcoinSeason/ui/btc-performance/btc-performance-heading.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { UiIcon, IconIds } from '@/shared/ui/icon';
+import { UiPosition } from '@/shared/ui/position';
+
+import AltcoinSeasonTimeFilter from '@/modules/widgets/altcoinSeason/ui/modals/altcoin-season-time-filter.vue';
 </script>
 
 <template>
@@ -7,14 +10,30 @@ import { UiIcon, IconIds } from '@/shared/ui/icon';
 		<div :class="classes.headingTitle">
 			BTC performance rank
 		</div>
-		<div :class="classes.headingTimeFrame">
-			90D
-			<ui-icon
-				:id="IconIds.DropdownDown"
-				:width="12"
-				:height="12"
-			/>
-		</div>
+
+		<ui-position
+			ref="btcPerformancePositionRef"
+			position="bottom-start"
+		>
+			<template #default="{ isVisible }">
+				<div :class="classes.TimeFrameSelector">
+					90D
+					<ui-icon
+						:id="IconIds.DropdownDown"
+						:width="12"
+						:height="12"
+						:class="[
+							classes.icon,
+							isVisible ? classes.icon__open : classes.icon__close
+						]"
+					/>
+				</div>
+			</template>
+
+			<template #content>
+				<altcoin-season-time-filter />
+			</template>
+		</ui-position>
 	</div>
 </template>
 
@@ -28,5 +47,18 @@ import { UiIcon, IconIds } from '@/shared/ui/icon';
 	align-items: center;
 	gap: 8px;
 	align-self: stretch;
+}
+
+.icon {
+	color: var(--icon-color-base-300);
+}
+
+.icon__open {
+	transform: rotate(180deg);
+	transition: transform 0.3s ease;
+}
+
+.icon__close {
+	transition: transform 0.3s ease;
 }
 </style>
