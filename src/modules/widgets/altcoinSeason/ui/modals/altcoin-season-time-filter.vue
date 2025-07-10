4c21983aacf34a8fe6a2d975a544b348af743940
Mark Solovev <msolovjev@planet9.cc>
2025-07-10 16:30:10 +0500

    refactor(altcoinSeason): replace modal filter with modal badge list
    - simplify modal structure by using dedicated badge list component
    - remove unused ModalFilter import and content slot

--- src/modules/widgets/altcoinSeason/ui/modals/altcoin-season-time-filter.vue
+++ src/modules/widgets/altcoinSeason/ui/modals/altcoin-season-time-filter.vue
@@ -1,25 +1,16 @@
 <script setup lang="ts">
-// import { ref } from 'vue';
-
-import { ModalFilter, ModalItemNumber } from '@/modules/widgets/base';
-// import { type Period } from '@/modules/widgets/altcoinSeason/model';
-
-// const period = ref<Period>('90D');
+import { ModalItemNumber, ModalBadgeList } from '@/modules/widgets/base';
 </script>
 
 <template>
-	<modal-filter>
+	<modal-badge-list>
 		<template #title>Filter</template>
-
-		<template #content>
-			<modal-item-number :value="1">1 day</modal-item-number>
-			<modal-item-number :value="2">7 days</modal-item-number>
-			<modal-item-number :value="3">30 days</modal-item-number>
-			<modal-item-number :value="4">90 days</modal-item-number>
-			<modal-item-number :value="5">1 year</modal-item-number>
-		</template>
-
-	</modal-filter>
+		<modal-item-number :value="1">1 day</modal-item-number>
+		<modal-item-number :value="2">7 days</modal-item-number>
+		<modal-item-number :value="3">30 days</modal-item-number>
+		<modal-item-number :value="4">90 days</modal-item-number>
+		<modal-item-number :value="5">1 year</modal-item-number>
+	</modal-badge-list>
 </template>
 
 <style module="classes">
