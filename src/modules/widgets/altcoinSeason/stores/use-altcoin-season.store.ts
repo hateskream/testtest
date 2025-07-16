63215e29ada366abb5b748dca9da76370b5a3892
Mark Solovev <msolovjev@planet9.cc>
2025-07-17 00:58:13 +0500

    fix: remove unused import

--- src/modules/widgets/altcoinSeason/stores/use-altcoin-season.store.ts
+++ src/modules/widgets/altcoinSeason/stores/use-altcoin-season.store.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref, computed, readonly } from 'vue';
+import { ref, computed } from 'vue';
 
 import type { IAltcoinSeasonConfig, IAltcoinSeasonRequest, AltcoinSeasonModuleKey } from '../model';
 import { useQueryAltcoinSeasonWidgetConfig, useQueryAltcoinSeason } from '../queries';
