d5d9df13f80e15f046d9363a4af4398400f8b066
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-07-24 14:50:55 +0300

    chore: refactoring version management

--- src/shared/composables/use-storage-version.ts
+++ src/shared/composables/use-storage-version.ts
@@ -4,18 +4,6 @@ const APP_VERSION = __APP_VERSION__ as string;
 export const useStorageVersion = () => {
 	const VERSION_KEY = 'app_version';
 
-	const clearAllCookies = () => {
-		document.cookie.split(';').forEach(cookie => {
-			const eqPos = cookie.indexOf('=');
-			const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-			document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
-		});
-	};
-
-	const clearLocalStorage = () => {
-		localStorage.clear();
-	};
-
 	const compareVersions = () => {
 		const savedVersion = localStorage.getItem(VERSION_KEY);
 		return savedVersion === APP_VERSION;
@@ -26,8 +14,6 @@ export const useStorageVersion = () => {
 	};
 
 	return {
-		clearAllCookies,
-		clearLocalStorage,
 		compareVersions,
 		updateVersion,
 	};
