cb98fb617780e34563bc59bffe075df2d36d5cf8
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-08-21 11:51:32 +0300

    feat: create composable

---
+++ src/modules/widgets/performance/composables/use-performance.ts
@@ -0,0 +1,50 @@
+import { computed, ref } from 'vue';
+
+import { type IState, getDefaultState } from '../model/state';
+import type { DateRange, Stock } from '../model/filters';
+import type { DisplayVariant } from '../model/display';
+
+export function usePerformance() {
+	const state = ref<IState>(getDefaultState());
+
+	const currentStock = computed({
+		get: () => state.value.stock,
+		set: (val: Stock) => {
+			state.value.stock = val;
+		},
+	});
+
+	const currentDate = computed({
+		get: () => state.value.date,
+		set: (val: DateRange) => {
+			state.value.date = val;
+		},
+	});
+
+	const currentDisplayVariant = computed({
+		get: () => state.value.displayVariant,
+		set: (val: DisplayVariant) => {
+			state.value.displayVariant = val;
+		},
+	});
+
+	const isCompactMode = computed({
+		get: () => state.value.isCompactMode,
+		set: (val: boolean) => {
+			state.value.isCompactMode = val;
+		},
+	});
+
+	function resetAllChanges() {
+		state.value = getDefaultState();
+	}
+
+	return {
+		currentStock,
+		currentDate,
+		currentDisplayVariant,
+		isCompactMode,
+
+		resetAllChanges,
+	};
+}
