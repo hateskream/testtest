c107435b77fafe69dd1bb5ef05f14567743a4a6e
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-17 19:27:36 +0300

    fix: cursor position during dnd

--- src/modules/dashboard-group/grid/ui/ghost-move-component.vue
+++ src/modules/dashboard-group/grid/ui/ghost-move-component.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, computed } from 'vue';
-import { throttle } from '@vexip-ui/utils';
+import { ref, onMounted, computed } from 'vue';
+import { useMouse } from '@vueuse/core';
 
 import GhostComponentBase from './ghost-component-base.vue';
-
-interface IPosition {
-	x: number;
-	y: number;
-}
 
 interface ISize {
 	height: number;
@@ -22,28 +17,17 @@ const props = defineProps<IGhostComponentProps>();
 
 const ghostComponentRef = ref<InstanceType<typeof GhostComponentBase> | null>(null);
 
-const position = ref<IPosition>({ x: 0, y: 0 });
-
 const sizeGhostComponent = ref<ISize>({ height: 0, width: 0 });
 
-/*
-	значение подобрано случайно
-	без него элемент криво центрируется
-	я склоняюсь к тому что это значение - размер курсора
-*/
-const MAGIC_NUMBER = 4;
+const { x, y } = useMouse();
 
 const styles = computed(() => ({
-	left: `${position.value.x - sizeGhostComponent.value.width / 2 - MAGIC_NUMBER}px`,
-	top: `${position.value.y - sizeGhostComponent.value.height / 2 - MAGIC_NUMBER}px`,
+	left: `${x.value - sizeGhostComponent.value.width / 2}px`,
+	top: `${y.value - sizeGhostComponent.value.height / 2}px`,
 	willChange: 'left, top',
 }));
 
-const trottledUpdatePosition = throttle(updatePosition);
-
 onMounted(() => {
-	window.addEventListener('mousemove', trottledUpdatePosition);
-
 	if (!ghostComponentRef.value) {
 		return;
 	}
@@ -53,37 +37,22 @@ onMounted(() => {
 		width: ghostComponentRef.value.$el.clientWidth,
 	};
 });
-
-onUnmounted(() => {
-	window.removeEventListener('mousemove', trottledUpdatePosition);
-});
-
-function updatePosition(event: MouseEvent) {
-	const scrollX = window.scrollX || window.pageXOffset;
-	const scrollY = window.scrollY || window.pageYOffset;
-
-	position.value = {
-		x: event.clientX - scrollX,
-		y: event.clientY - scrollY,
-	};
-}
 </script>
 
 <template>
-	<div
-
-		:class="classes.root"
-	>
+	<teleport to="#app">
 		<ghost-component-base
 			ref="ghostComponentRef"
 			:style="styles"
 			:title="props.title"
+			:class="classes.root"
 		/>
-	</div>
+	</teleport>
 </template>
 
 <style module="classes">
 .root {
-	position: fixed;
+	position: absolute;
+	z-index: 9999;
 }
 </style>
