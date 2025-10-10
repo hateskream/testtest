6e8ff13f7fccd04431dc75ff39c8ba16d3ff4bd8
Baykov Ivan <45362427+efcolipt@users.noreply.github.com>
2025-10-11 00:14:47 +0700

    refactor: change icon names for consistency

--- src/modules/dashboards-curtain/ui/dashboards-curtain-component.vue
+++ src/modules/dashboards-curtain/ui/dashboards-curtain-component.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import type { IWidgetPreset } from '@/modules/dashboard-group/core';
 import { WidgetType } from '@/modules/dashboard-group/core';
 import { IconIds, UiIcon } from '@/shared/ui/icon';
-import type { IWidgetPreset } from '@/modules/dashboard-group/core';
 
 import DraggableElement from './draggable-element.vue';
 import DashboardComponent from './dashboard-component.vue';
@@ -10,7 +10,7 @@ import SearchComponent from './search-component.vue';
 const WIDGET_TYPE_TO_ICON: Partial<Record<WidgetType, IconIds>> = {
 	[WidgetType.Market]: IconIds.LogoWidgetMarket,
 	[WidgetType.FearGreed]: IconIds.LogoWidgetFearAndGreat,
-	[WidgetType.Price]: IconIds.LogoWidgetPrice,
+	[WidgetType.Price]: IconIds.LogoWidgetPriceList,
 	[WidgetType.News]: IconIds.LogoWidgetNews,
 	[WidgetType.Watchlist]: IconIds.LogoWidgetWatchlist,
 	[WidgetType.MarketCap]: IconIds.LogoWidgetMarketCap,
@@ -19,7 +19,7 @@ const WIDGET_TYPE_TO_ICON: Partial<Record<WidgetType, IconIds>> = {
 	[WidgetType.Heatmap]: IconIds.Heatmap,
 	[WidgetType.AltcoinSeason]: IconIds.LogoWidgetAltcoinSeason,
 	[WidgetType.TopIndices]: IconIds.LogoWidgetTopIndices,
-	[WidgetType.ChartPrice]: IconIds.LogoWidgetChartPrice,
+	[WidgetType.ChartPrice]: IconIds.LogoWidgetPriceChart,
 	[WidgetType.Exchange]: IconIds.LogoWidgetExchange,
 	[WidgetType.EthGas]: IconIds.LogoWidgetEthGas,
 	[WidgetType.Performance]: IconIds.LogoWidgetPerfomance,
