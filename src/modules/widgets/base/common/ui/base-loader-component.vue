0c3805a90949ff19896a28be8c733ae54ec6f7cc
Andrej Shaev <andrejshaev@gmail.com>
2025-10-21 21:38:35 +0700

    feat(widgets): create BaseLoadingComponent for consistent loader management

---
+++ src/modules/widgets/base/common/ui/base-loader-component.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { computed, useTemplateRef } from 'vue';
+import { useElementSize } from '@vueuse/core';
+
+import { UiSkeletonGroup } from '@/shared/ui/skeleton';
+
+interface IBaseLoaderComponentProps {
+	rowHeight?: number;
+	minLines?: number;
+	maxLines?: number;
+	rowCount?: number;
+	gap?: number;
+	fitMode?: 'shrink' | 'expand';
+}
+
+const props = withDefaults(defineProps<IBaseLoaderComponentProps>(), {
+	minLines: 2,
+	maxLines: Infinity,
+	rowCount: undefined,
+	rowHeight: 64,
+	gap: 10,
+	fitMode: 'shrink',
+});
+
+const { height: contentHeight } = useElementSize(useTemplateRef('contentRef'));
+
+const calculatedLinesCount = computed(() => {
+	const rawCount = contentHeight.value / (props.rowHeight + props.gap);
+
+	if (props.fitMode === 'shrink') {
+		return Math.floor(rawCount);
+	}
+
+	return Math.ceil(rawCount);
+});
+
+const optimizedLinesCount = computed(() => {
+	return Math.max(
+		props.minLines,
+		Math.min(props.maxLines, props.rowCount || calculatedLinesCount.value),
+	);
+});
+
+const lineHeight = computed(() => props.rowHeight + 'px');
+</script>
+
+<template>
+	<div :class="classes.container">
+		<slot name="before"></slot>
+		<div ref="contentRef" :class="classes.content">
+			<slot :lines-count="optimizedLinesCount">
+				<ui-skeleton-group :count="optimizedLinesCount" :height="lineHeight" />
+			</slot>
+		</div>
+		<slot name="after"></slot>
+	</div>
+</template>
+
+<style module="classes">
+.container {
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+}
+
+.content {
+	height: 100%;
+	overflow-y: hidden;
+}
+</style>
