e9b1b253438eae96ae07f95195568093626e1275
Andrej Shaev <andrejshaev@gmail.com>
2025-11-12 19:58:40 +0700

    fix(ticker-selector): resolve conflict

--- src/modules/ticker-selector/ui/modal-ticker-selector-with-badge.vue
+++ src/modules/ticker-selector/ui/modal-ticker-selector-with-badge.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, nextTick, useTemplateRef } from 'vue';
+import { computed } from 'vue';
 
 import { ModalFilter } from './components/modal';
 import { IconIds, UiIcon } from '@/shared/ui/icon';
@@ -8,7 +8,7 @@ import { ACTIVE_TICKER_LIST_COUNT_SHOW, getMappedRow, type ITickerEmits } from '
 import { ModalBadge } from '@/modules/widgets/base';
 import { marketToLabel, MarketType } from '@/modules/market';
 import { SymbolType } from '@/modules/cell';
-import { ModalFilterTickerIcon } from '@/modules/ticker-selector/ui/components/modal';
+import { ModalFilterTickerIcon, ModalFilterTickerLabel } from '@/modules/ticker-selector/ui/components/modal';
 
 interface IProps {
 	selectionMode?: 'single' | 'multiple';
@@ -17,6 +17,7 @@ interface IProps {
 	searchPlaceholder?: string;
 	marketTypes?: MarketType[];
 	autofocus?: boolean;
+	showLabel?: boolean;
 }
 
 const props = withDefaults(defineProps<IProps>(), {
@@ -44,16 +45,6 @@ const selectedTickersMapped = computed(() => {
 
 const previewLabel = computed(() => marketToLabel[props.marketTypes[0]]);
 
-const filterRef = useTemplateRef('filter');
-
-function onChangeVisible(state: boolean) {
-	if (state && props.autofocus) {
-		nextTick(() => {
-			filterRef.value?.focusSearch();
-		});
-	}
-}
-
 function getSize(symbol: SymbolType | null | undefined) {
 	if (props.displayVariant === 'new' || !symbol) {
 		return 14;
@@ -72,43 +63,52 @@ function getPadding(symbol: SymbolType | null | undefined) {
 </script>
 
 <template>
-	<modal-badge :display-variant="props.displayVariant" @change-visible="onChangeVisible">
+	<modal-badge :display-variant="props.displayVariant">
 		<template #title>
-			<div
-				v-if="selectedTickersMapped.length > 0"
-				:class="classes.iconsWrapper"
-			>
+			<div :class="classes.header">
 				<div
-					v-for="item in selectedTickersMapped"
-					:key="item.tickerId"
-					:class="classes.iconsItem"
+					v-if="selectedTickersMapped.length > 0"
+					:class="classes.iconsWrapper"
 				>
-					<modal-filter-ticker-icon
-						:src-image="item.srcImage"
-						:type="item.symbolType"
-						:ticker="item.ticker"
-						:size="getSize(item.symbolType)"
-						:padding="getPadding(item.symbolType)"
-						:display-variant="props.displayVariant"
-					/>
+					<div
+						v-for="item in selectedTickersMapped"
+						:key="item.tickerId"
+						:class="classes.iconsItem"
+					>
+						<modal-filter-ticker-icon
+							:src-image="item.srcImage"
+							:type="item.symbolType"
+							:ticker="item.ticker"
+							:size="getSize(item.symbolType)"
+							:padding="getPadding(item.symbolType)"
+							:display-variant="props.displayVariant"
+						/>
+					</div>
+					<template v-if="selectedTickers.length > ACTIVE_TICKER_LIST_COUNT_SHOW">
+						+ {{ selectedTickers.length - ACTIVE_TICKER_LIST_COUNT_SHOW }}
+					</template>
 				</div>
-				<template v-if="selectedTickers.length > ACTIVE_TICKER_LIST_COUNT_SHOW">
-					+ {{ selectedTickers.length - ACTIVE_TICKER_LIST_COUNT_SHOW }}
-				</template>
+				<div v-if="selectedTickers.length === 0">
+					{{previewLabel}}
+				</div>
+				<div v-else-if="props.showLabel" :class="classes.labels">
+					<modal-filter-ticker-label
+						v-for="ticker in selectedTickersMapped"
+						:key="ticker.tickerId"
+						:ticker="ticker"
+					/>
+					<span v-if="selectedTickersMapped.length > 1" :class="classes.labelsDelimiter">,</span>
+				</div>
+				<ui-icon
+					:id="IconIds.DropdownDown"
+					width="12"
+					height="12"
+				/>
 			</div>
-			<div v-if="selectedTickers.length === 0">
-				{{previewLabel}}
-			</div>
-			<ui-icon
-				:id="IconIds.DropdownDown"
-				width="12"
-				height="12"
-			/>
 		</template>
 		<template #content>
 			<modal-filter
 				v-if="data"
-				ref="filter"
 				v-model="selectedTickers"
 				:selection-mode="props.selectionMode"
 				:tickers="data.tickers"
@@ -116,6 +116,7 @@ function getPadding(symbol: SymbolType | null | undefined) {
 				:search-placeholder="props.searchPlaceholder"
 				:market-types="props.marketTypes"
 				:display-variant="displayVariant"
+				:autofocus="props.autofocus"
 				@select="emits('select', $event)"
 				@unselect="emits('unselect', $event)"
 				@select-all="emits('selectAll', $event)"
@@ -125,6 +126,12 @@ function getPadding(symbol: SymbolType | null | undefined) {
 </template>
 
 <style module="classes">
+.header {
+	display: flex;
+	gap: 4px;
+	align-items: center;
+}
+
 .iconsItem {
 	margin-left: -12px;
 	border-radius: 100%;
@@ -138,5 +145,16 @@ function getPadding(symbol: SymbolType | null | undefined) {
 	display: flex;
 	align-items: center;
 	gap: 4px;
+}
+
+.labels {
+	max-width: 120px;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+}
+
+.labelsDelimiter {
+	margin-right: 3px;
 }
 </style>
