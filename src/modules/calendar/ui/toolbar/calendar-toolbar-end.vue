4b3c5fd6eeffd188a3cf800f54bd64d475467d53
Mark Solovev <msolovjev@planet9.cc>
2025-11-13 14:07:19 +0500

    refactor: change calendar icon sizings

--- src/modules/calendar/ui/toolbar/calendar-toolbar-end.vue
+++ src/modules/calendar/ui/toolbar/calendar-toolbar-end.vue
@@ -16,39 +16,42 @@ const emits = defineEmits<{
 
 <template>
 	<div :class="classes.toolbarEnd">
-		<button
-			:class="[classes.nav]"
-			@click="emits('prev-week')"
-		>
-			<ui-icon
-				:id="IconIds.DropdownDown"
-				width="12"
-				height="12"
-				:class="['dropdown-icon']"
-			/>
-		</button>
+		<div :class="classes.controls">
+			<button
+				:class="[classes.nav]"
+				@click="emits('prev-week')"
+			>
+				<ui-icon
+					:id="IconIds.DropdownDown"
+					width="12"
+					height="12"
+					:class="['dropdown-icon']"
+				/>
+			</button>
 
-		<button
-			:class="[classes.nav, isSameCalendarDay(props.baseDate, props.initialDate) && classes.hidden]"
-			@click="emits('reset-week')"
-		>
-			<ui-icon
-				:id="IconIds.CalendarV2"
-				width="20px"
-				height="20px"
-				:class="['dropdown-icon', classes.calendarIcon]"
-			/>
-			<span :class="classes.redDot" />
-		</button>
+			<button
+				:class="[classes.nav, isSameCalendarDay(props.baseDate, props.initialDate) && classes.hidden]"
+				@click="emits('reset-week')"
+			>
+				<ui-icon
+					:id="IconIds.CalendarV2"
+					width="20px"
+					height="20px"
+					:class="['dropdown-icon', classes.calendarIcon]"
+				/>
+				<span :class="classes.redDot" />
+			</button>
 
-		<button :class="[classes.nav, classes.rotated]" @click="emits('next-week')">
-			<ui-icon
-				:id="IconIds.DropdownDown"
-				width="12"
-				height="12"
-				:class="['dropdown-icon']"
-			/>
-		</button>
+			<button :class="[classes.nav, classes.rotated]" @click="emits('next-week')">
+				<ui-icon
+					:id="IconIds.DropdownDown"
+					width="12"
+					height="12"
+					:class="['dropdown-icon']"
+				/>
+			</button>
+		</div>
+
 		<div v-if="$slots.default" :class="classes.label">
 			<slot />
 		</div>
@@ -68,13 +71,21 @@ const emits = defineEmits<{
 	margin-left: 12px;
 	font-weight: 600;
 	font-size: 0.875rem;
+	text-align: right;
+}
+
+.controls {
+	display: flex;
+	gap: 2px;
 }
 
 .nav {
 	position: relative;
 	display: grid;
-	width: 2rem;
-	height: 2rem;
+	width: 20px;
+	height: 20px;
+	padding: 0;
+	line-height: 0;
 	color: inherit;
 	background: transparent;
 	border: none;
@@ -97,15 +108,11 @@ const emits = defineEmits<{
 
 .redDot {
 	position: absolute;
-	right: 7px;
-	bottom: 6px;
+	right: 0;
+	bottom: 0;
 	width: 6px;
 	height: 6px;
 	background-color: rgb(230 0 0 / 100%);
 	border-radius: 50%;
-}
-
-.nav:hover {
-	background: #1a1a1a;
 }
 </style>
