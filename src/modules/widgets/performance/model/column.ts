843c2d1ea3b139cac4d54e5326707bb2dc9e0d90
Vasiliy <Vasiliy>
2025-11-20 00:44:46 +0700

    [table/common] Performance and indicies widgets fixes

--- src/modules/widgets/performance/model/column.ts
+++ src/modules/widgets/performance/model/column.ts
@@ -15,9 +15,9 @@ export const ALL_COLUMNS: ITableColumn[] =
 				isShow: true,
 				isDraggable: true,
 				extended: true,
-				maxWidth: '200px',
-				minWidth: '200px',
-				width: '200px',
+				maxWidth: 200,
+				minWidth: 200,
+				width: 200,
 			},
 		],
 	);
