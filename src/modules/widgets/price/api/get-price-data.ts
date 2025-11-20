24d120188ffd1420ff279648d0308a4fbc50a6b2
Andrej Shaev <andrejshaev@gmail.com>
2025-11-20 10:36:02 +0700

    chore(price-list): disable chart mock

--- src/modules/widgets/price/api/get-price-data.ts
+++ src/modules/widgets/price/api/get-price-data.ts
@@ -16,7 +16,7 @@ import { generateRows } from '@/shared/mock';
 import { delay } from '@/shared/lib';
 import { MarketType } from '@/modules/market';
 
-const IS_USE_MOCK = true;
+const IS_USE_MOCK = false;
 
 interface IGetPriceRequest {
 	market: MarketType;
--- src/modules/widgets/price/ui/common/cell-component.vue
+++ src/modules/widgets/price/ui/common/cell-component.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-import MockChart from '@/assets/images/mock/transparent-chart.svg';
 import { IconIds, UiIcon } from '@/shared/ui/icon';
 import { UiTransitionFade } from '@/shared/ui/transition';
 import { forexTickerIcon, tickerIcon } from '@/shared/ui/ticker';
@@ -97,10 +96,12 @@ const priceChange = computed(() => getPercentData(props.ticker.changePrice24hPer
 						:class="classes.chart"
 					>
 						<img
-							:src="MockChart"
-							style="width: 90px; height: 36px; object-fit: contain;"
+							v-if="props.ticker.price24hChart.src?.length"
+							:src="props.ticker.price24hChart.src"
+							style="width: 90px; height: 36px; object-fit: cover;"
 							alt="chart"
 							fetchpriority="high"
+							:class="classes.chartImage"
 						/>
 					</div>
 				</ui-transition-fade>
@@ -259,5 +260,9 @@ const priceChange = computed(() => getPercentData(props.ticker.changePrice24hPer
 .enterFrom,
 .leaveTo {
 	opacity: 0;
+}
+
+.chartImage {
+	mask-image: linear-gradient(to right, transparent, #000000 30%);
 }
 </style>
