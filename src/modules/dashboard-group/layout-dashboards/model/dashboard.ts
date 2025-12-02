cc52bdd05bcdf2896d105f093b04eef36d3c0aa0
Vasiliy <Vasiliy>
2025-12-02 08:50:52 +0700

    deleted payroll & unemployment from test place

--- src/modules/dashboard-group/layout-dashboards/model/dashboard.ts
+++ src/modules/dashboard-group/layout-dashboards/model/dashboard.ts
@@ -22,18 +22,6 @@ const presets: Record<PresetName, ISectionPreset[]> = {
 			width: 560,
 			widgets: [
 				{
-					defaultState: 'none',
-					height: 400,
-					widgetType: WidgetType.UnemploymentRate,
-					defaultDisplayVariant: 'default',
-				},
-				{
-					defaultState: 'none',
-					height: 400,
-					widgetType: WidgetType.NonfarmPayrolls,
-					defaultDisplayVariant: 'default',
-				},
-				{
 					defaultState: 'stock',
 					height: 465,
 					widgetType:  WidgetType.ChartPrice,
