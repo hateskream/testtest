03bf36a24d2b8a581430b6d4ddf7895550e1bc6e
Andrej Shaev <andrejshaev@gmail.com>
2025-12-16 05:08:16 +0700

    feat(ui): add UiText component for token-based typography

---
+++ src/shared/ui/text/token.ts
@@ -0,0 +1,9 @@
+export type TextToken =
+	'text-50-r' | 'text-50-r-up'
+	| 'text-100-r' | 'text-100-b' | 'text-100-r-up'
+	| 'text-200-b' | 'text-200-r'
+	| 'text-300-r' | 'text-300-b'
+	| 'text-400-r' | 'text-400-b'
+	| 'text-500-b' | 'text-500-r'
+	| 'title-100' | 'title-200' | 'title-300' | 'title-400' | 'title-500' | 'title-600';
+
