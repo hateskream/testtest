34b17e54aab3fb11df7feccc86cb3b6ffc25189a
Mark Solovev <msolovjev@planet9.cc>
2026-01-27 15:59:01 +0500

    refactor: remove legacy ticker selector components

--- src/modules/ticker-selector/index.ts
+++ src/modules/ticker-selector/index.ts
@@ -2,8 +2,8 @@ export * from './model';
 export * from './composables';
 
 export { default as ModalTickerSelectorLegacy } from './ui/modal-ticker-selector-legacy.vue';
-export { default as ModalTickerSelectorWithBadgeLegacy } from './ui/modal-ticker-selector-with-badge-legacy.vue';
 
-export { default as TickerSelectorModal } from './new/ticker-selector-modal.vue';
-export { default as TickerSelectorModalWithBadge } from './new/ticker-selector-modal-with-badge.vue';
+export { default as TickerSelectorModal } from './ui/ticker-selector-modal.vue';
+export { default as TickerSelectorModalWithBadge } from './ui/ticker-selector-modal-with-badge.vue';
+
 export { fetchTickers } from './api/fetch-tickers.ts';
