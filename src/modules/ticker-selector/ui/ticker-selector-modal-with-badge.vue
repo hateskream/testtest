238d8715d05231103bba7d3d5847da8f5b088edb
Mark Solovev <msolovjev@planet9.cc>
2026-01-27 15:59:59 +0500

    refactor: rename ticker-selector/new to ticker-selector/ui

---
+++ src/modules/ticker-selector/ui/ticker-selector-modal-with-badge.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts" generic="M extends readonly MarketType[]">
+import { computed, useTemplateRef } from 'vue';
+
+import { ModalBadgeDropdown } from '@/modules/widgets/base';
+import { MarketType } from '@/modules/market';
+import { type IMarketTickerItem, type ITickerItem, SelectionMode } from '@/modules/ticker-selector';
+
+import ModalBadgePreview from './components/badge/modal-badge-preview.vue';
+import TickerSelectorModal from '@/modules/ticker-selector/new/ticker-selector-modal.vue';
+
+const props = withDefaults(defineProps<{
+	enabledMarkets: M;
+	selectionMode: SelectionMode;
+	enableSelectAll?: boolean;
+	enableMarketTickers?: boolean;
+
+	displayVariant: 'new' | 'default';
+	showLabel?: boolean;
+	showIcon?: boolean;
+	closeOnSelect?: boolean;
+}>(), {
+	enableMarketTickers: false,
+	showLabel: true,
+	showIcon: true,
+});
+
+const searchQuery = defineModel<string>('search-query', {
+	default: '',
+});
+const selectedMarkets = defineModel<M>('selected-markets', {
+	default: [],
+});
+const selectedTickers = defineModel<ITickerItem[]>('selected-tickers', {
+	default: [],
+});
+const excludedTickers = defineModel<ITickerItem[]>('excluded-tickers', {
+	default: [],
+});
+const selectedMarketTickers = defineModel<IMarketTickerItem<M[number]>[]>('selected-market-tickers', {
+	default: [],
+});
+
+const paddingLeft = computed(() => {
+	if (!props.showIcon || props.displayVariant === 'default') {
+		return undefined;
+	}
+
+	const isHasMarketTicker = props.enableMarketTickers
+		? !!selectedMarketTickers.value.length : false;
+
+	return selectedTickers.value.length || isHasMarketTicker ? '4px' : undefined;
+});
+
+const dropdownRef = useTemplateRef('dropdown');
+
+function closeDropdown() {
+	dropdownRef.value?.close();
+}
+
+function onChanged() {
+	if (props.closeOnSelect && props.selectionMode === 'single') {
+		closeDropdown();
+	}
+}
+</script>
+
+<template>
+	<modal-badge-dropdown
+		ref="dropdown"
+		:display-variant="props.displayVariant"
+		:padding-left="paddingLeft"
+	>
+		<template #title>
+			<modal-badge-preview
+				:selected-tickers="selectedTickers"
+				:selected-markets="selectedMarkets"
+				:excluded-tickers="excludedTickers"
+				:selected-market-tickers="selectedMarketTickers"
+				:display-variant="props.displayVariant"
+				:show-icon="props.showIcon"
+				:show-label="props.showLabel"
+			/>
+		</template>
+
+		<template #content>
+			<ticker-selector-modal
+				v-model:search-query="searchQuery"
+				v-model:selected-markets="selectedMarkets"
+				v-model:excluded-tickers="excludedTickers"
+				v-model:selected-tickers="selectedTickers"
+				v-model:selected-market-tickers="selectedMarketTickers"
+				:enable-market-tickers="props.enableMarketTickers"
+				:selection-mode="props.selectionMode"
+				:enable-select-all="props.enableSelectAll"
+				:enabled-markets="props.enabledMarkets"
+				:display-variant="props.displayVariant"
+				@update:selected-tickers="onChanged"
+			/>
+		</template>
+	</modal-badge-dropdown>
+</template>
+
+<style scoped>
+
+</style>
