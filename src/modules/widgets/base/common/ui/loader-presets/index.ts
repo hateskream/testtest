f589c06d603a379e89a05de86c711e6fbbb0e755
Andrej Shaev <andrejshaev@gmail.com>
2026-01-28 19:20:04 +0700

    fix: change imports src, delete useless component

--- src/modules/widgets/base/common/ui/loader-presets/index.ts
+++ src/modules/widgets/base/common/ui/loader-presets/index.ts
@@ -1,8 +1,9 @@
 import BarSkeleton from './bar-skeleton.vue';
-import ChestSkeleton from './chest-skeleton.vue';
+import ChartSkeleton from './chart-skeleton.vue';
 import HeatmapSkeleton from './heatmap-skeleton.vue';
 import IndicatorSkeleton from './indicator-skeleton.vue';
 import ListSkeleton from './list-skeleton.vue';
 import TableSkeleton from './table-skeleton.vue';
 import TileSkeleton from './tile-skeleton.vue';
-export { BarSkeleton, ChestSkeleton, HeatmapSkeleton, IndicatorSkeleton, ListSkeleton, TableSkeleton, TileSkeleton };
+
+export { BarSkeleton, ChartSkeleton, HeatmapSkeleton, IndicatorSkeleton, ListSkeleton, TableSkeleton, TileSkeleton };
--- src/modules/widgets/key-indicators/index.ts
+++ src/modules/widgets/key-indicators/index.ts
@@ -1,1 +1,1 @@
-export { default as TickerKeyIndicatorsWidget } from './ui/ticker-page/ticker-key-indicators-widget.vue';
+export { default as TickerKeyIndicatorsWidget } from './ui/ticker/ticker-key-indicators-widget.vue';
