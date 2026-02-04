97dbc23b98895c7883ccdb22e5cf97ce3b317f5b
Andrej Shaev <andrejshaev@gmail.com>
2026-02-04 10:34:45 +0700

    chore: change modal-badge-title background states

--- src/modules/widgets/base/modal/ui/modal-badge-title-component.vue
+++ src/modules/widgets/base/modal/ui/modal-badge-title-component.vue
@@ -42,19 +42,16 @@ const props = withDefaults(defineProps<IProps>(), {
 	min-height: 32px;
 	padding: 0 var(--pl, 12px);
 	color: var(--color);
-	background: var(--bg);
+	background-color: var(--bg);
 	border-radius: 18px;
 	cursor: pointer;
 	gap: 4px;
-}
-
-.title.old {
-	transition: color 0.2s ease, background 0.2s ease;
+	transition: color 0.2s ease, background-color 0.2s ease;
 }
 
 .title.old:hover {
 	color: var(--control-secondary-object, #adadad);
-	background: var(--color-bg-hover-base-300-hover, rgb(64 64 64 / 40%));
+	background-color: var(--color-bg-hover-base-300-hover, rgb(64 64 64 / 40%));
 }
 
 .new {
@@ -64,8 +61,16 @@ const props = withDefaults(defineProps<IProps>(), {
 	padding-right: 6px;
 	padding-left: var(--pl, 10px);
 	color: rgb(255 255 255 / 96%);
-	background: rgb(73 73 80 / 70%);
+	background-color: var(--bg-100, rgb(73 73 80 / 32%));
 	border-radius: var(--radius-radius-s12-24, 9.2px);
 	gap: 3px;
+}
+
+.title.new:hover {
+	background-color: var(--base-50, rgb(73 73 80 / 52%));
+}
+
+.title.new:active {
+	background-color: var(--bg-500, rgb(73 73 80 / 70%));
 }
 </style>
