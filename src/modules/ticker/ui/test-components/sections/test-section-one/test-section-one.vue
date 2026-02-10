5b3728b3ea59b0944e4e94b60e05361542c436e4
Vasiliy Bondarev <vbondarev@planet9.cc>
2026-02-10 18:03:55 +0700

    lint-fix

--- src/modules/ticker/ui/test-components/sections/test-section-one/test-section-one.vue
+++ src/modules/ticker/ui/test-components/sections/test-section-one/test-section-one.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import {computed} from "vue";
-import {isFeatureEnabled} from "@/shared/lib";
-const isPricerPerformanceEnabled = computed(()=> isFeatureEnabled('TICKER_WIDGET_PRICE_PERFORMANCE_ENABLED'))
+import { computed } from 'vue';
+
+import { isFeatureEnabled } from '@/shared/lib';
+const isPricerPerformanceEnabled = computed(()=> isFeatureEnabled('TICKER_WIDGET_PRICE_PERFORMANCE_ENABLED'));
 </script>
 
 <template>
--- src/modules/ticker/ui/test-components/sections/test-section-three/test-section-three.vue
+++ src/modules/ticker/ui/test-components/sections/test-section-three/test-section-three.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 import { ActivityMetricsTickerWidget } from '@/modules/widgets/activity-metrics';
 import { TickerLinksWidget } from '@/modules/widgets/links';
 import { TickerKeyIndicatorsWidget } from '@/modules/widgets/key-indicators';
 import { useTickerContext } from '@/modules/ticker/composables';
 import { BaseTickerWidgetContent, BaseTickerWidgetHeader, BaseTickerWidgetWrapper } from '@/modules/widgets/base';
-import {isFeatureEnabled} from "@/shared/lib";
-import {computed} from "vue";
+import { isFeatureEnabled } from '@/shared/lib';
 
 const { tickerId } = useTickerContext();
-const isShowTickerIndicators = computed(()=> isFeatureEnabled('TICKER_WIDGET_INDICATORS_ENABLED'))
-const isShowActivityMetrics = computed(()=> isFeatureEnabled('TICKER_WIDGET_ACTIVITY_METRICS_ENABLED'))
+const isShowTickerIndicators = computed(()=> isFeatureEnabled('TICKER_WIDGET_INDICATORS_ENABLED'));
+const isShowActivityMetrics = computed(()=> isFeatureEnabled('TICKER_WIDGET_ACTIVITY_METRICS_ENABLED'));
 const isShowSection = computed(()=> isShowTickerIndicators.value || isShowActivityMetrics.value);
 </script>
 
 <template>
-	<div :class="classes.section" v-if="isShowSection">
+	<div v-if="isShowSection" :class="classes.section">
 		<activity-metrics-ticker-widget :meta="{ tickerId }" />
 		<base-ticker-widget-wrapper>
 			<base-ticker-widget-header>Insights</base-ticker-widget-header>
