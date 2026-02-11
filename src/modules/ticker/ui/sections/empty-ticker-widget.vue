97d0da419176fc062bcfbf5d4ac220c0d1c5b724
Andrej Shaev <andrejshaev@gmail.com>
2026-02-11 16:32:26 +0700

    feat(ticker): add empty widget message

--- src/modules/ticker/ui/sections/empty-ticker-widget.vue
+++ src/modules/ticker/ui/sections/empty-ticker-widget.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { ITickerWidgetMeta } from '../../models';
 import { BaseTickerWidgetHeader, BaseTickerWidgetWrapper } from '@/modules/widgets/base';
+import { UiTag } from '@/shared/ui/tag';
 
 interface IProps {
 	meta: ITickerWidgetMeta;
@@ -10,7 +11,24 @@ const props = defineProps<IProps>();
 </script>
 
 <template>
-	<base-ticker-widget-wrapper>
+	<base-ticker-widget-wrapper :class="classes.wrapper">
 		<base-ticker-widget-header>{{ props.meta.name }}</base-ticker-widget-header>
+		<div :class="classes.content">
+			<ui-tag color="neutral">Widget under development</ui-tag>
+		</div>
 	</base-ticker-widget-wrapper>
 </template>
+
+<style module="classes">
+.wrapper {
+	display: flex;
+	flex-direction: column;
+}
+
+.content {
+	display: flex;
+	flex-grow: 1;
+	justify-content: center;
+	align-items: center;
+}
+</style>
