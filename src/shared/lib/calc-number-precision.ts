7da12a2b88d6b5bf8b9e1b210df6daf897f8db94
Andrej Shaev <andrejshaev@gmail.com>
2026-02-18 04:43:09 +0700

    chore: change number precision

--- src/shared/lib/calc-number-precision.ts
+++ src/shared/lib/calc-number-precision.ts
@@ -4,7 +4,7 @@ export function calcNumberPrecision(value: number, min = 1, max = 8): number {
 	}
 
 	const order = Math.floor(Math.log10(Math.abs(value)));
-	const precision = Math.max(0, -order) + 1;
+	const precision = Math.max(0, -order) + 2;
 
 	return Math.min(Math.max(precision, min), max);
 }
