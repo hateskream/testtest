b54e3ed813da5ababa92df49b96808482f12631f
Andrej Shaev <andrejshaev@gmail.com>
2026-03-25 02:31:15 +0700

    feat: add feature-flags for financials sector widgets

--- src/modules/ticker/ui/sections/stock/financials-section.vue
+++ src/modules/ticker/ui/sections/stock/financials-section.vue
@@ -2,18 +2,24 @@
 import { useTickerContext } from '@/modules/ticker/composables';
 import { TickerAnnualReturnsWidget } from '@/modules/widgets/annual-returns';
 import { AnnualMarginTrendsTickerWidget } from '@/modules/widgets/annual-margin-trends';
+import { isFeatureEnabled } from '@/shared/lib';
 
 const { tickerId } = useTickerContext();
+
+const annualReturnsIsEnabled = isFeatureEnabled('TICKER_WIDGET_ANNUAL_MARGIN_TRENDS_ENABLED');
+const annualMarginTrendsIsEnabled = isFeatureEnabled('TICKER_WIDGET_ANNUAL_RETURNS_ENABLED');
 </script>
 
 <template>
 	<div :class="classes.section">
 		<div :class="classes.metrics">
 			<ticker-annual-returns-widget
+				v-if="annualReturnsIsEnabled"
 				:class="classes.metricsItem"
 				:meta="{ tickerId, name: 'Annual Returns' }"
 			/>
 			<annual-margin-trends-ticker-widget
+				v-if="annualMarginTrendsIsEnabled"
 				:class="classes.metricsItem"
 				:meta="{ tickerId, name: 'Annual Margin Trends' }"
 			/>
