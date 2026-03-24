d72a2d471bde4121782dc4a35b845902097580a4
Andrej Shaev <andrejshaev@gmail.com>
2026-03-24 19:03:04 +0700

    feat: add Current Sentiment feature-flag

--- src/modules/ticker/ui/sections/stock/insights-and-activity/tab-insights-content.vue
+++ src/modules/ticker/ui/sections/stock/insights-and-activity/tab-insights-content.vue
@@ -4,15 +4,21 @@ import { EconomicOutlineTickerWidget } from '@/modules/widgets/economic-outline'
 import { CurrentSentimentTickerWidget } from '@/modules/widgets/current-sentiment';
 import { useTickerContext } from '@/modules/ticker/composables';
 import { TickerBaseListDivider, TickerBaseTabContent } from '../../../base';
+import { isFeatureEnabled } from '@/shared/lib';
 
 const { tickerId } = useTickerContext();
+
+const currentSentimentIsEnabled = isFeatureEnabled('TICKER_WIDGET_CURRENT_SENTIMENT_ENABLED');
 </script>
 
 <template>
 	<ticker-base-tab-content>
-		<current-sentiment-ticker-widget :meta="{ tickerId, name: 'Current Sentiment' }" />
+		<current-sentiment-ticker-widget
+			v-if="currentSentimentIsEnabled"
+			:meta="{ tickerId, name: 'Current Sentiment' }"
+		/>
 		<ticker-base-list-divider />
-		<economic-outline-ticker-widget :meta="{ tickerId, name: 'Economic Outline' }" />
+		<economic-outline-ticker-widget  :meta="{ tickerId, name: 'Economic Outline' }" />
 		<ticker-base-list-divider />
 		<ticker-key-indicators-widget :meta="{ tickerId, name: 'Key Indicators' }" />
 	</ticker-base-tab-content>
